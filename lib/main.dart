2b871bf5ab2257ae6df824037048c8f04f2014a1
Dev-Mubashir <dev.mubashirurrehman@gmail.com>
2023-01-23 00:25:01 +0500

    After completing sign in & sign up UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,11 @@ import 'package:socialaux/responsive/mobile_screen_layout.dart';
 import 'package:socialaux/responsive/responsive_layout_screen.dart';
 import 'package:socialaux/responsive/web_screen_layout.dart';
 import 'package:socialaux/screens/login_screen.dart';
+import 'package:socialaux/screens/signup_screen.dart';
 import 'package:socialaux/utils/colors.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized;
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       //   mobileScreenLayout: MobileScreenLayout(),
       //   webScreenLayout: WebScreenLayout(),
       // ),
-      home: LoginScreen(),
+      home: SignupScreen(),
     );
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,1 +1,159 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:socialaux/utils/colors.dart';
+import 'package:socialaux/widgets/text_field_input.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+
+              ///svg image
+              SvgPicture.asset(
+                'assets/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+
+              const SizedBox(height: 64),
+
+              //circular widget to accept and show our selected files
+              Stack(
+                children: [
+                  const CircleAvatar(
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                        'https://cdn1.vectorstock.com/i/1000x1000/45/65/user-icon-human-person-sign-vector-20444565.jpg'),
+                  ),
+                  Positioned(
+                      bottom: -10,
+                      left: 80,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.add_a_photo),
+                      ))
+                ],
+              ),
+
+              const SizedBox(height: 24),
+
+              //test field input for username
+              TextFieldInput(
+                  textEditingController: _usernameController,
+                  hintText: "Enter your username",
+                  textInputType: TextInputType.text),
+
+              const SizedBox(height: 24),
+
+              //text field input for email
+              TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: "Enter your email",
+                  textInputType: TextInputType.emailAddress),
+
+              const SizedBox(height: 24),
+
+              //text field input for password
+              TextFieldInput(
+                textEditingController: _passwordController,
+                hintText: "Enter your password",
+                textInputType: TextInputType.text,
+                isPass: true,
+              ),
+
+              const SizedBox(height: 24),
+
+              //text field input for bio
+              TextFieldInput(
+                  textEditingController: _bioController,
+                  hintText: "Enter your bio",
+                  textInputType: TextInputType.text),
+              const SizedBox(height: 24),
+
+              const SizedBox(height: 24),
+
+              //button login
+              InkWell(
+                child: Container(
+                  child: Text("Log in"),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4),
+                      ),
+                    ),
+                    color: blueColor,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 14),
+
+              Flexible(
+                child: Container(),
+                flex: 2,
+              ),
+
+              //transitioning to signing up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text("Don't have an account?"),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
